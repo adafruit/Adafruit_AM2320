592b4ca5a669635aa790fe68d1fb1b99b626b558
dean <deanm1278@gmail.com>
2018-03-07 13:03:54 -0500

    add docs

--- Adafruit_AM2320.cpp
+++ Adafruit_AM2320.cpp
@@ -1,39 +1,87 @@
-// Adafruit AM2320 Temperature & Humidity Unified Sensor Library
-// Copyright (c) 2018 Adafruit Industries
-// Author: Limor Fried
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+/*!
+ * @file Adafruit_AM2320.cpp
+ *
+ * @mainpage Adafruit AM2320 Temperature & Humidity Unified Sensor driver
+ *
+ * @section intro_sec Introduction
+ *
+ * This is the documentation for Adafruit's AM2320 driver for the
+ * Arduino platform.  It is designed specifically to work with the
+ * Adafruit AM2320 breakout: https://www.adafruit.com/products/xxxx
+ *
+ * These sensors use I2C to communicate, 2 pins (SCL+SDA) are required
+ * to interface with the breakout.
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * @section dependencies Dependencies
+ *
+ * This library depends on <a href="https://github.com/adafruit/Adafruit_Sensor">
+ * Adafruit_Sensor</a> being present on your system. Please make sure you have
+ * installed the latest version before using this library.
+ *
+ * @section author Author
+ *
+ * Written by Limor Fried for Adafruit Industries.
+ *
+ * @section license License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
 
 #include "Adafruit_AM2320.h"
 
+  /**************************************************************************/
+  /*! 
+      @brief  Instantiates a new AM2320 class
+      @param theI2C Optional pointer to a TwoWire object that should be used for I2C communication. Defaults to &Wire.
+      @param tempSensorId the id that should be used for the temperature sensor. Defaults to -1
+      @param humiditySensorId the id that should be used for the humidity sensor. Defaults to -1
+  */
+  /**************************************************************************/
 Adafruit_AM2320::Adafruit_AM2320(TwoWire *theI2C, int32_t tempSensorId, int32_t humiditySensorId):
   _temp(this, tempSensorId),
   _humidity(this, humiditySensorId),
   _i2c(theI2C)
 {}
 
+/**************************************************************************/
+/*! 
+    @brief  Setups the hardware
+    @return true
+*/
+/**************************************************************************/
 bool Adafruit_AM2320::begin() {
   _i2caddr = 0x5C;  // fixed addr
   _i2c->begin();
   return true;
 }
 
+/**************************************************************************/
+/*! 
+    @brief  read the temperature from the device
+    @return the temperature reading as a floating point value
+*/
+/**************************************************************************/
 float Adafruit_AM2320::readTemperature() {
   uint16_t t = readRegister16(AM2320_REG_TEMP_H);
   if (t == 0xFFFF) return NAN;
@@ -42,6 +90,12 @@ float Adafruit_AM2320::readTemperature() {
   return ft / 10.0;
 }
 
+/**************************************************************************/
+/*! 
+    @brief  read the humidity from the device
+    @return the humidity reading as a floating point value
+*/
+/**************************************************************************/
 float Adafruit_AM2320::readHumidity() {
   uint16_t h = readRegister16(AM2320_REG_HUM_H);
   if (h == 0xFFFF) return NAN;
@@ -50,6 +104,13 @@ float Adafruit_AM2320::readHumidity() {
   return fh / 10.0;
 }
 
+/**************************************************************************/
+/*! 
+    @brief  read 2 bytes from a hardware register
+    @param reg the register to read
+    @return the read value as a 2 byte unsigned integer
+*/
+/**************************************************************************/
 uint16_t Adafruit_AM2320::readRegister16(uint8_t reg) {
   // wake up
   Wire.beginTransmission(_i2caddr);
@@ -96,7 +157,14 @@ uint16_t Adafruit_AM2320::readRegister16(uint8_t reg) {
   return ret;
 }
 
-
+/**************************************************************************/
+/*! 
+    @brief  perfor a CRC check to verify data
+    @param buffer the pointer to the data to check
+    @param nbytes the number of bytes to calculate the CRC over
+    @return the calculated CRC
+*/
+/**************************************************************************/
 uint16_t Adafruit_AM2320::crc16(uint8_t *buffer, uint8_t nbytes) {
   uint16_t crc = 0xffff;
   for (int i=0; i<nbytes; i++) {
@@ -114,19 +182,45 @@ uint16_t Adafruit_AM2320::crc16(uint8_t *buffer, uint8_t nbytes) {
   return crc;
 }
 
+/**************************************************************************/
+/*! 
+    @brief  set the name of the sensor
+    @param sensor a pointer to the sensor
+*/
+/**************************************************************************/
 void Adafruit_AM2320::setName(sensor_t* sensor) {
   strncpy(sensor->name, "AM2320", sizeof(sensor->name) - 1);
 }
 
+/**************************************************************************/
+/*! 
+    @brief  set minimum delay to a fixed value of 2 seconds
+    @param sensor a pointer to the sensor
+*/
+/**************************************************************************/
 void Adafruit_AM2320::setMinDelay(sensor_t* sensor) {
   sensor->min_delay = 2000000L;  // 2 seconds (in microseconds)
 }
 
+/**************************************************************************/
+/*! 
+    @brief  create a temperature sensor instance
+    @param parent the pointer to the parent sensor
+    @param id the id value for the sensor
+*/
+/**************************************************************************/
 Adafruit_AM2320::Temperature::Temperature(Adafruit_AM2320* parent, int32_t id):
   _parent(parent),
   _id(id)
 {}
 
+/**************************************************************************/
+/*! 
+    @brief read the temperature from the device and populate a sensor_event_t with the value
+    @param event a pointer to the event to populate
+    @return true
+*/
+/**************************************************************************/
 bool Adafruit_AM2320::Temperature::getEvent(sensors_event_t* event) {
   // Clear event definition.
   memset(event, 0, sizeof(sensors_event_t));
@@ -140,6 +234,12 @@ bool Adafruit_AM2320::Temperature::getEvent(sensors_event_t* event) {
   return true;
 }
 
+/**************************************************************************/
+/*! 
+    @brief populate a sensor_t with data for this sensor
+    @param sensor a pointer to the sensor_t to populate
+*/
+/**************************************************************************/
 void Adafruit_AM2320::Temperature::getSensor(sensor_t* sensor) {
   // Clear sensor definition.
   memset(sensor, 0, sizeof(sensor_t));
@@ -158,11 +258,25 @@ void Adafruit_AM2320::Temperature::getSensor(sensor_t* sensor) {
   sensor->resolution  = 2.0F;
 }
 
+/**************************************************************************/
+/*! 
+    @brief  create a humidity sensor instance
+    @param parent the pointer to the parent sensor
+    @param id the id value for the sensor
+*/
+/**************************************************************************/
 Adafruit_AM2320::Humidity::Humidity(Adafruit_AM2320* parent, int32_t id):
   _parent(parent),
   _id(id)
 {}
 
+/**************************************************************************/
+/*! 
+    @brief read the humidity from the device and populate a sensor_event_t with the value
+    @param event a pointer to the event to populate
+    @return true
+*/
+/**************************************************************************/
 bool Adafruit_AM2320::Humidity::getEvent(sensors_event_t* event) {
   // Clear event definition.
   memset(event, 0, sizeof(sensors_event_t));
@@ -176,6 +290,12 @@ bool Adafruit_AM2320::Humidity::getEvent(sensors_event_t* event) {
   return true;
 }
 
+/**************************************************************************/
+/*! 
+    @brief populate a sensor_t with data for this sensor
+    @param sensor a pointer to the sensor_t to populate
+*/
+/**************************************************************************/
 void Adafruit_AM2320::Humidity::getSensor(sensor_t* sensor) {
   // Clear sensor definition.
   memset(sensor, 0, sizeof(sensor_t));
