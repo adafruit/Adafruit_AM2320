8e13636107ecd114680e75b087b2bc290e9f85e4
Doxygen CI <ci-arduino@invalid>
2021-09-17 17:04:04 +0000

    Deploy docs to GitHub Pages from commit 15acc4b6eb
    Commit: 15acc4b6eba44f84f32951e6ca6d94bbbfebebc0
    GitHub Actions run: 1246305748

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['humidity',['Humidity',['../class_adafruit___a_m2320_1_1_humidity.html',1,'Adafruit_AM2320::Humidity'],['../class_adafruit___a_m2320_1_1_humidity.html#afb02af51fc56c2cfcf618dfbdb4abb1d',1,'Adafruit_AM2320::Humidity::Humidity()'],['../class_adafruit___a_m2320.html#a08e6cd57bd14453c3cefd9ece3e1bc2a',1,'Adafruit_AM2320::humidity()']]]
+  ['humidity',['Humidity',['../class_adafruit___a_m2320_1_1_humidity.html',1,'Adafruit_AM2320::Humidity'],['../class_adafruit___a_m2320.html#a08e6cd57bd14453c3cefd9ece3e1bc2a',1,'Adafruit_AM2320::humidity()'],['../class_adafruit___a_m2320_1_1_humidity.html#afb02af51fc56c2cfcf618dfbdb4abb1d',1,'Adafruit_AM2320::Humidity::Humidity()']]]
 ];
